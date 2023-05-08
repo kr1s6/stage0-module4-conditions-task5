17a6f91c7a695a67d7f92834d6bd976167ca49b2
kr1s6 <krzysztof.bielkiewicz@interia.pl>
2023-05-08 20:10:57 +0200

    Finished.

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        int x = year%100 == 0 && year%400 != 0? 0 : 1;
+        switch (x){
+            case 1 -> System.out.println("leap");
+            case 0 -> System.out.println("not leap");
+        }
     }
 }
